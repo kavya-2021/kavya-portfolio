988d2c4384eba56f2593cf01a59b87490c069845
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-02-09 18:54:40 +0530

    new changes
    new changes that are required

--- src/components/Home/Home3.js
+++ src/components/Home/Home3.js
@@ -85,13 +85,6 @@ function Home3() {
             &nbsp;Download CV
           </Button>
         </Row>
-
-        {/* <Row className="resume">
-          <Document file={resumeLink}>
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row> */}
-
       </Container>
     </Container>
   );
