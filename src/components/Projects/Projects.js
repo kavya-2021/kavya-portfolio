e14aad89c2a12828ceb03a2b1e35aa9a414c5449
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-02-09 23:24:06 +0530

    new update
    project section github repo path added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -37,7 +37,7 @@ function Projects() {
               isBlog={false}
               title="Desert Cart Clone"
               description= "An e-commerce website for purchasing and importing products available for import from different countries.Products can be shipped to 163+ countries Wide variety of products. There are filters that can filter products depending on importing country and delivery time. This model was able to achieve most of the features that are similiar to reference website"
-              link="https://github.com/soumyajit4419/Chatify"
+              link="https://github.com/kavya-2021/DesertCart_Clone"
             />
           </Col>
 
@@ -47,7 +47,7 @@ function Projects() {
               isBlog={false}
               title="Purplle - using HTML"
               description="An e-commerce website for purchasing fashion and skincare products. Features unique to this website are , it has a very Dynamic Home page, Advanced filtering options. Skin Type testing and suggestions related to products based on test results.Is has a very vibrant wishlist page and wide range of products. Navigation to required products can be found"
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              link="https://github.com/mayuriwasu1/purplle_clone"
             />
           </Col>
         </Row>
